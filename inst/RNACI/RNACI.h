ef03d756f66185403b02b3ea5546883aba6011ed
Drew Schmidt <schmidt@math.utk.edu>
2015-06-16 19:24:08 -0400

    fixed RNACI header

--- inst/RNACI/RNACI.h
+++ inst/RNACI/RNACI.h
@@ -8,6 +8,8 @@
 #ifndef __RNACI_H__
 #define __RNACI_H__
 
+#define RNACI_VERSION 0.2.0
+
 
 #include <R.h>
 #include <Rinternals.h>
@@ -187,6 +189,55 @@ static inline SEXP __Rmatalloc(int m, int n, char *type, int init)
   
   UNPROTECT(1);
   return RET;
+}
+
+
+// floats.c
+
+static inline int fis_zerof(float x)
+{
+  const float abs_epsf = 1.1f * FLT_EPSILON;
+  if (fabsf(x) < abs_epsf*FLT_MIN)
+    return true;
+  else
+    return false;
+}
+
+static inline int fis_zero(double x)
+{
+  const double abs_eps = 1.1 * DBL_EPSILON;
+  if (fabs(x) < abs_eps*DBL_MIN)
+    return true;
+  else
+    return false;
+}
+
+static inline int fequalsf(float x, float y)
+{
+  const float abs_epsf = 1.1f * FLT_EPSILON;
+  const double abs_eps = 1.1 * DBL_EPSILON;
+  const double diff = fabsf(x - y);
+  
+  if (x == y)
+    return true;
+  else if (x == 0.0f || y == 0.0f || diff < FLT_MIN)
+    return diff < (abs_epsf*FLT_MIN);
+  else
+    return diff/(fabsf(x) + fabsf(y)) < abs_epsf;
+}
+
+static inline int fequals(double x, double y)
+{
+  const float abs_epsf = 1.1f * FLT_EPSILON;
+  const double abs_eps = 1.1 * DBL_EPSILON;
+  const double diff = fabs(x - y);
+  
+  if (x == y)
+    return true;
+  else if (x == 0.0 || y == 0.0 || diff < DBL_MIN)
+    return diff < (abs_eps*DBL_MIN);
+  else
+    return diff/(fabs(x) + fabs(y)) < abs_eps;
 }
 
 
