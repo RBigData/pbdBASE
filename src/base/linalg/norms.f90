7922f6bbe88042cfa384da7ee224134c84dff68a
Drew Schmidt <wrathematics@gmail.com>
2016-07-15 12:43:41 -0400

    rename auxil.f90 to norms.f90

--- src/base/linalg/norms.f90
+++ src/base/linalg/norms.f90
@@ -2,7 +2,7 @@
 ! License, v. 2.0. If a copy of the MPL was not distributed with this
 ! file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-! Copyright 2013, Schmidt
+! Copyright 2013, 2016, Schmidt
 
 
 ! pdlange subroutine wrapper for use with f77_call in c
@@ -17,39 +17,36 @@ subroutine matnorm(value, norm, m, n, a, ia, ja, desca)
   integer             lwork, bl, ioffa, iamar, nprow, npcol, myprow, mypcol
   ! dynamic
   double precision, allocatable :: work(:)
-  ! parameters
-  integer             i0
-  parameter ( i0 = 0 )
   ! functions
   double precision    pdlange
   integer             indxg2p, numroc
   
   
   if (norm .eq. "M" .or. norm .eq. "F") then
-    lwork = i0
+    lwork = 0
   else 
     call blacs_gridinfo(desca(2), nprow, npcol, myprow, mypcol)
     
-    if (norm .eq. "O") then
+    if (norm .eq. "O" .or. norm .eq. "1") then
       bl = desca(5)
       ioffa = mod(ja-1, bl)
       
-      iamar = indxg2p(ia, bl, mypcol, i0, npcol)
+      iamar = indxg2p(ia, bl, mypcol, 0, npcol)
       
-      lwork = numroc(n+ioffa, bl, mypcol, i0, npcol)
+      lwork = numroc(n+ioffa, bl, mypcol, 0, npcol)
     else if (norm .eq. "I") then
       bl = desca(6)
       ioffa = mod(ia-1, bl)
       
-      iamar = indxg2p(ia, bl, myprow, i0, nprow)
+      iamar = indxg2p(ia, bl, myprow, 0, nprow)
       
-      lwork = numroc(m+ioffa, bl, myprow, i0, nprow)
+      lwork = numroc(m+ioffa, bl, myprow, 0, nprow)
     end if
   end if
   
-  allocate (work(lwork))
+  allocate(work(lwork))
   value = pdlange(norm, m, n, a, ia, ja, desca, work)
-  deallocate (work)
+  deallocate(work)
   
   return
 end subroutine
@@ -69,9 +66,6 @@ subroutine condnum(norm, m, n, a, ia, ja, desca, rcond, info)
   ! local
   integer             lwork, liwork, lipiv
   double precision    anorm, tmp
-  ! parameters
-  integer             in1
-  parameter ( in1 = -1 )
   ! dynamic
   double precision, allocatable :: work(:)
   integer, allocatable :: iwork(:), ipiv(:)
@@ -84,27 +78,28 @@ subroutine condnum(norm, m, n, a, ia, ja, desca, rcond, info)
   
   ! step 2:  factor a=lu
   lipiv = desca(9) + desca(5) ! locr(m_a)+ mb_a
-  allocate (ipiv(lipiv))
+  allocate(ipiv(lipiv))
   
   call pdgetrf(m, n, a, ia, ja, desca, ipiv, info)
   
-  if (info.ne.0) then
+  if (info .ne. 0) then
+    deallocate(ipiv)
     return
   end if
   
   ! step 3:  call pdgecon
-  call pdgecon(norm, n, a, ia, ja, desca, anorm, rcond, tmp, in1, liwork, in1, info)
+  call pdgecon(norm, n, a, ia, ja, desca, anorm, rcond, tmp, -1, liwork, -1, info)
   
   lwork = int(tmp)
-  allocate (work(lwork))
-  allocate (iwork(liwork))
+  
+  allocate(work(lwork))
+  allocate(iwork(liwork))
   
   call pdgecon(norm, n, a, ia, ja, desca, anorm, rcond, work, lwork, iwork, liwork, info)
   
-  deallocate (ipiv)
-  deallocate (iwork)
-  deallocate (work)
+  deallocate(ipiv)
+  deallocate(iwork)
+  deallocate(work)
   
   return
 end subroutine
-
